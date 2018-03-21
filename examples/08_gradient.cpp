fcd48b539adcd643106d506320a70cce56bd576b
namark <nshan.nnnn@gmail.com>
2018-03-21 15:06:11 +0400

    Minor fixes in example 08.

--- examples/08_gradient.cpp
+++ examples/08_gradient.cpp
@@ -10,16 +10,16 @@ int main() try
 
 	SDL_Init(SDL_INIT_EVERYTHING);
 	{
-		software_window win("Image", {600,600}, window::flags::borderless);
+		software_window win("Gradient", {600,600}, window::flags::borderless);
 
 		surface canvas(win.surface().size(), pixel_format(pixel_format::type::rgb24));
 		auto pixels = std::get<pixel_writer<rgb_pixel, surface::byte>>(canvas.pixels());
 
 		auto start = rgb_vector(0.0f,1.0f,1.0f);
 		auto end = rgb_vector(1.0f,0.0f,1.0f);
+		rgb_vector color;
 		for(point2D position{}; position < pixels.size(); ++position.y())
 		{
-			rgb_vector color;
 			for(; position < pixels.size(); ++position.x())
 			{
 				auto ratio = vector2D(position) / vector2D(pixels.size());
