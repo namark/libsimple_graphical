cbffc02c51ca4abe471f716c5d924b49b89e5861
namark <namark@disroot.org>
2019-03-17 02:37:41 +0400

    Minor cleanup.

--- source/simple/graphical/initializer.cpp
+++ source/simple/graphical/initializer.cpp
@@ -1,7 +1,5 @@
 #include "initializer.h"
-#include "simple/support/enum.hpp"
 
-using namespace simple;
 using namespace simple::graphical;
 
 initializer::initializer() : sdlcore::initializer(sdlcore::system_flag::video)
--- source/simple/graphical/initializer.h
+++ source/simple/graphical/initializer.h
@@ -1,7 +1,6 @@
 #ifndef SIMPLE_GRAPHICAL_INITIALIZER_H
 #define SIMPLE_GRAPHICAL_INITIALIZER_H
 #include "simple/sdlcore/initializer.h"
-#include "simple/support/enum_flags_operators.hpp"
 
 namespace simple::graphical
 {
