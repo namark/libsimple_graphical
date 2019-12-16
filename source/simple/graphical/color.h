b9f05caf0651388eb23f695fc986de8d17881b93
namark <namark@disroot.org>
2019-12-17 03:32:57 +0400

    Just another TODO.

--- source/simple/graphical/color.h
+++ source/simple/graphical/color.h
@@ -19,6 +19,7 @@ namespace simple::graphical
 		constexpr explicit color(uint32_t raw) : raw(raw) {};
 		color(const pixel_format& format, const rgb_pixel& values);
 		color(const pixel_format& format, const rgba_pixel& values);
+		// TODO: should be private, a public interface should return a read only range of bytes, or maybe leave it to reinterpret_cast
 		explicit operator uint32_t() const;
 	};
 
